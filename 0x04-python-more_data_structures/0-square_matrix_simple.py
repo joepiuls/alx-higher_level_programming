6731eeb7bc00cf42faa1fffbaa0196d5b0bce17c
joepiuls <john71688@gmail.com>
2023-09-13 15:43:35 +0000

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        for j in i:
+            res.append(i[j**2])
+    return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This project is about more on data structures (list, dictionaries, map, lamda, reduce, filter) done on alx software egineering program.
