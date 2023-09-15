35a3129211e8179b0d8a9117e78e3f640168d821
joepiuls <john71688@gmail.com>
2023-09-15 21:10:08 +0000

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 
-def square_matrix_map(matrix=[]):
-    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary.keys()):
+        if a_dictionary[i] is value:
+            del a_dictionary[i]
+    return a_dictionary
