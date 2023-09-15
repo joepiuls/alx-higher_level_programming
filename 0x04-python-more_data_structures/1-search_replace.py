424da89185b83c1250a80a9579c2b9c9dd0b9990
joepiuls <john71688@gmail.com>
2023-09-15 12:18:05 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-	else:
-	    new_list.append(element)
-    return new_list
+    list(map(lambda e: replace if e == search else e,  my_list))
