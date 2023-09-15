013b5120596f65644836e27188f2aa82d85730c5
joepiuls <john71688@gmail.com>
2023-09-15 22:20:34 +0000

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
-    list(map(lambda e: replace if e == search else e,  my_list))
+    return list(map(lambda e: replace if e == search else e, my_list))
