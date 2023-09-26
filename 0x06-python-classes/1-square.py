5f470b3ac5d05eacabdd88db1080b5de424742cf
joepiuls <john71688@gmail.com>
2023-09-26 12:08:31 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py by Joseph John
+"""Defines a square """
+
+
+class Square:
+    """Represents the square"""
+
+    def __init__(self, size):
+        """Initializing the square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This project is about python classes done at alx software engineering program.
